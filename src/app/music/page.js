774d019816196aade86452c8b544758f13f80e07
Lee Jaeho <ljhasdr@gmail.com>
2024-10-12 21:53:52 +0900

    수정

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -1,8 +1,16 @@
+import "./player.css"
 
 export default function Home() {
   return (
     <div>
-      asdasdas
+      <iframe
+        allow="autoplay *; encrypted-media *;"
+        className="bg-transparent w-[360px] absolute left-[50%] translate-x-[-50%] max-w-[600px] top-[50%] translate-y-[-50%]"
+        frameborder="0"
+        height="450"
+        sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+        src="https://embed.music.apple.com/kr/playlist/24-3/pl.u-RRbVv11TeMl12ky?l=en"
+      />
     </div>
   );
 }
